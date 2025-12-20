9903b25c0c9279fe8bed2b3512c48fed76520df3
Francis Leo <drypasta34@gmail.com>
2025-12-20 12:07:09 +0800

    Add task completion feature to task list
    Implemented the ability to mark tasks as completed by clicking the 'Complete' button. Updated the UI to visually indicate completed tasks with a different background color and opacity.

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const taskInput = document.querySelector(".task-wrapper input");
 const addTaskButton = document.querySelector(".task-wrapper button");
+const taskListUL = document.querySelector(".task-list ul")
 
 function addTaskHandler(){
     const currentTask = taskInput.value;
 
-    console.log(currentTask);
+    if (currentTask.trim() === ""){
+        console.log("Input field is empty")
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.classList.add("task");
+
+    li.innerHTML = `
+        <span>${currentTask}</span>
+        <button type="button" class="complete-btn">Complete</button>
+    `;
+
+    taskListUL.appendChild(li);
 
     taskInput.value = "";
 }
 
-addTaskButton.addEventListener('click', addTaskHandler)
+
+addTaskButton.addEventListener('click', addTaskHandler);
+
+taskListUL.addEventListener('click', function(event){
+    if (event.target.classList.contains("complete-btn")){
+        const taskItem = event.target.closest(".task");
+        taskItem.classList.toggle("completed")
+    }
+});
